1cc0ea1832fab2efacb63e2faca4922eb40b9c33
VaidikKhurana <vaidikkhurana@gmail.com>
2024-05-26 12:35:16 +0530

    Uploaded Files

---
+++ src/prvict/algorithms/str_int.py
@@ -0,0 +1,54 @@
+def str_int(input, u_data):
+ for char in input:
+    if char == 'a':
+        u_data.append(1)
+    elif char == 'b':
+        u_data.append(2)
+    elif char == 'c':
+        u_data.append(3)
+    elif char == 'd':
+        u_data.append(4)
+    elif char == 'e':
+        u_data.append(5)
+    elif char == 'f':
+        u_data.append(6)
+    elif char == 'g':
+        u_data.append(7)
+    elif char == 'h':
+        u_data.append(8)
+    elif char == 'i':
+        u_data.append(9)
+    elif char == 'j':
+        u_data.append(10)
+    elif char == 'k':
+        u_data.append(11)
+    elif char == 'l':
+        u_data.append(12)
+    elif char == 'm':
+        u_data.append(13)
+    elif char == 'n':
+        u_data.append(14)
+    elif char == 'o':
+        u_data.append(15)
+    elif char == 'p':
+        u_data.append(16)
+    elif char == 'q':
+        u_data.append(17)
+    elif char == 'r':
+        u_data.append(18)
+    elif char == 's':
+        u_data.append(19)
+    elif char == 't':
+        u_data.append(20)
+    elif char == 'u':
+        u_data.append(21)
+    elif char == 'v':
+        u_data.append(22)
+    elif char == 'w':
+        u_data.append(23)
+    elif char == 'x':
+        u_data.append(24)
+    elif char == 'y':
+        u_data.append(25)
+    elif char == 'z':
+        u_data.append(26)
